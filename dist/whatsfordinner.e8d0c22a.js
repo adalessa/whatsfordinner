6de885fe59ef99049ee58d20bf4a812f46560030
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2018-10-22 22:55:25 -0300

    commit agian

--- dist/whatsfordinner.e8d0c22a.js
+++ dist/whatsfordinner.e8d0c22a.js
@@ -7856,7 +7856,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '52359' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '56748' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
